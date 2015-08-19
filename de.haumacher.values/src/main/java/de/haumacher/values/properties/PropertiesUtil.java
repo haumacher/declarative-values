a91f063cbc34c185cedfc2ac37c80b0315386d0f
Bernhard Haumacher <haui@haumacher.de>
2015-08-19 18:49:03 +0200

    Adjusted file headers with license and copyright information.

--- de.haumacher.values/src/main/java/de/haumacher/values/properties/PropertiesUtil.java
+++ de.haumacher.values/src/main/java/de/haumacher/values/properties/PropertiesUtil.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012-2015, Bernhard Haumacher. 
+ * All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 package de.haumacher.values.properties;
 
 import java.io.FileInputStream;
@@ -7,6 +26,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import de.haumacher.values.Initializer;
 import de.haumacher.values.Parser;
@@ -14,8 +34,6 @@ import de.haumacher.values.Property;
 import de.haumacher.values.Value;
 import de.haumacher.values.ValueDescriptor;
 import de.haumacher.values.ValueFactory;
-
-import java.util.Properties;
 
 
 public class PropertiesUtil {
